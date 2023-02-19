0ee72dc3f61d7116db23b22f1ae5d6aa03309544
vladimir <slawgorodskij@mail.ru>
2023-02-19 21:02:56 +0700

    fixed eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
 import './App.css';
-import {useEffect, useState} from "react";
-import Form from "components/Form/Form";
-import Messages from "components/Messages/Messages";
+import { useEffect, useState } from 'react';
+import Form from 'components/Form/Form';
+import Messages from 'components/Messages/Messages';
 
 function App() {
-    const [messageList, setMessageList] = useState([]);
-    useEffect(() => {
-        let lastMessage = messageList[messageList.length - 1];
+  const [messageList, setMessageList] = useState([]);
+  useEffect(() => {
+    let lastMessage = messageList[messageList.length - 1];
 
-        if (lastMessage && lastMessage.author !== 'Ваш любимый бот') {
-            const message = {
-                'text': `${lastMessage.author} написал новое сообщение`,
-                'author': 'Ваш любимый бот',
-            }
-            setTimeout(() => {
-                setMessageList((arrayMessage) => [...arrayMessage, message]);
-            }, 1500)
-
-        }
-    }, [messageList])
-    return (
-        <div className="App">
-            <Messages messages={messageList}/>
-            <Form addMessage={setMessageList}/>
-        </div>
-    );
+    if (lastMessage && lastMessage.author !== 'Ваш любимый бот') {
+      const message = {
+        text: `${lastMessage.author} написал новое сообщение`,
+        author: 'Ваш любимый бот',
+      };
+      setTimeout(() => {
+        setMessageList((arrayMessage) => [...arrayMessage, message]);
+      }, 1500);
+    }
+  }, [messageList]);
+  return (
+    <div className="App">
+      <Messages messages={messageList} />
+      <Form addMessage={setMessageList} />
+    </div>
+  );
 }
-
 export default App;
--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import style from './messages.module.css'
+import style from './messages.module.css';
 
-const Messages = ({messages}) => {
-    return (
-        <div className={style.block}>
-            {messages.map(item => {
-                return (
-                    <div
-                        key={item.id}>
-                        <p><span>{item.author}</span> {item.text}</p>
-                    </div>
-                )
-            })}
-        </div>
-    );
+const Messages = ({ messages }) => {
+  return (
+    <div className={style.block}>
+      {messages.map((item) => {
+        return (
+          <div key={item.id}>
+            <p>
+              <span>{item.author}</span> {item.text}
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Messages;
