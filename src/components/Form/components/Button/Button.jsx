55c35f13dee95426d60da891a8785d267f2dc8bd
vladimir <slawgorodskij@mail.ru>
2023-02-21 21:13:35 +0700

    внес изменения после занятия

--- src/components/Form/components/Button/Button.jsx
+++ src/components/Form/components/Button/Button.jsx
@@ -1,12 +1,9 @@
-import style from "./button.module.css";
+import style from './button.module.css';
 
-export const Button = ({disabled, label}) =>{
-    return (
-        <button
-            disabled={disabled}
-            className={style.button}
-        >
-            {label}
-        </button>
-    )
-}
+export const Button = ({ disabled, label }) => {
+  return (
+    <button disabled={disabled} className={style.button}>
+      {label}
+    </button>
+  );
+};
