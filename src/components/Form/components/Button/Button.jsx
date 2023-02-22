9c51b1be680682059282ffb1d0b25188304a3924
vladimir <slawgorodskij@mail.ru>
2023-02-22 23:16:07 +0700

    код с урока

--- src/components/Form/components/Button/Button.jsx
+++ src/components/Form/components/Button/Button.jsx
@@ -1,9 +1,12 @@
 import style from './button.module.css';
 
-export const Button = ({ disabled, label }) => {
-  return (
-    <button disabled={disabled} className={style.button}>
-      {label}
-    </button>
-  );
+export const Button = ({label, disabled}) => {
+    return (
+        <button
+            disabled={disabled}
+            className={style.button}
+        >
+            {label}
+        </button>
+    );
 };
