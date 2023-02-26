593f8b63eaef6d63fbfc5369f743a8956b8a9519
vladimir <slawgorodskij@mail.ru>
2023-02-26 22:03:18 +0700

    добавлено ДЗ

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,45 @@
 import './App.css';
-import React, {FC, useEffect, useState} from 'react';
-import {Form} from 'components/Form';
-import {MessageList} from 'components/MessageList';
-import {AUTHOR, Message, Messages} from 'src/types';
-import {Sidebar} from "components/Sidebar";
-import {Grid} from "@material-ui/core";
+import React, { FC, useEffect, useState } from 'react';
+import { Form } from 'components/Form';
+import { MessageList } from 'components/MessageList';
+import { AUTHOR, Message, Messages } from 'src/types';
+import { Sidebar } from 'components/Sidebar';
+import { Grid } from '@material-ui/core';
 
 export const App: FC = () => {
-    const [messageList, setMessageList] = useState<Messages>([]);
-    const addMessage = (newMessage: Message) => {
-        setMessageList((prevMessage) => [...prevMessage, newMessage]);
-    };
-    useEffect(() => {
-        const lastMessage = messageList[messageList.length - 1];
+  const [messageList, setMessageList] = useState<Messages>([]);
+  const addMessage = (newMessage: Message) => {
+    setMessageList((prevMessage) => [...prevMessage, newMessage]);
+  };
+  useEffect(() => {
+    const lastMessage = messageList[messageList.length - 1];
 
-        if (lastMessage && lastMessage.author !== AUTHOR.BOT) {
-            const timeout = setTimeout(() => {
-                addMessage({
-                    id: Date.now(),
-                    text: `${lastMessage.author} написал новое сообщение`,
-                    author: AUTHOR.BOT,
-                });
-            }, 1500);
-            return () => clearTimeout(timeout);
-        }
-    }, [messageList]);
-    return (
-        <Grid container spacing={2}>
-            <Grid item xs={2} container>
-                <Sidebar/>
-            </Grid>
-            <Grid item xs={12} sm container>
-                <Grid item xs container direction="column" spacing={2}>
-                    <Grid item xs>
-                        <MessageList messages={messageList}/>
-                    </Grid>
-                    <Grid item>
-                        <Form addMessage={addMessage}/>
-                    </Grid>
-                </Grid>
-            </Grid>
+    if (lastMessage && lastMessage.author !== AUTHOR.BOT) {
+      const timeout = setTimeout(() => {
+        addMessage({
+          id: Date.now(),
+          text: `${lastMessage.author} написал новое сообщение`,
+          author: AUTHOR.BOT,
+        });
+      }, 1500);
+      return () => clearTimeout(timeout);
+    }
+  }, [messageList]);
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={2} container>
+        <Sidebar />
+      </Grid>
+      <Grid item xs={12} sm container>
+        <Grid item xs container direction="column" spacing={2}>
+          <Grid item xs>
+            <MessageList messages={messageList} />
+          </Grid>
+          <Grid item>
+            <Form addMessage={addMessage} />
+          </Grid>
         </Grid>
-    );
+      </Grid>
+    </Grid>
+  );
 };
--- src/components/Sidebar/Sidebar.test.tsx
+++ src/components/Sidebar/Sidebar.test.tsx
@@ -1,17 +1,15 @@
-import {render, screen} from '@testing-library/react';
-import {Sidebar} from './Sidebar';
+import { render, screen } from '@testing-library/react';
+import { Sidebar } from './Sidebar';
 import '@testing-library/jest-dom';
 
 describe('Sidebar', () => {
+  it('render component', () => {
+    render(<Sidebar />);
+    screen.debug();
+  });
 
-    it('render component', () => {
-        render(<Sidebar/>);
-        screen.debug();
-    });
-
-    it('message is empty', () => {
-        render(<Sidebar/>);
-        expect(screen.queryAllByTestId('blockMessage').length).toBe(0);
-    });
-
+  it('message is empty', () => {
+    render(<Sidebar />);
+    expect(screen.queryAllByTestId('blockMessage').length).toBe(0);
+  });
 });
